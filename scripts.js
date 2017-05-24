01ddea7a12e89757b83a06f3a1bc257153909808
Lukasz Strozik <lukaszstrozik@gmail.com>
2017-05-24 15:19:47 +0200

    added jquery option

--- scripts.js
+++ scripts.js
@@ -19,3 +19,25 @@ function getJoke() {
 }
 // get joke immediately after page load
 getJoke();
+
+
+/* AJAX with jQuery
+
+    var url = 'http://api.icndb.com/jokes/random';
+
+    var $button = $('#get-joke').click(function() {
+	getJoke();
+    });
+
+    var $paragraph = $('#joke');
+
+    function getJoke() {
+        $.ajax({
+	       method: 'GET',
+	       url: url, //here's a weird construction, but on the left we have the name of the parameter, and to the right is the name of the variable that holds the value
+	       success: function(res) {
+		         $paragraph.text(res.value.joke);
+	            }
+
+        });
+    }
